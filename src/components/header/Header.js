00eca2b7f4847c7faba7f44b81056fc84f71e101
Umidjon Dedaxanov <macbook@MacBook-Pro-macbook.local>
2022-03-18 12:02:57 +0500

    hedaer fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,8 +8,12 @@ import { ReactComponent as MoneyIcon } from "../../assets/UI-icons/money-lg.svg"
 import { ReactComponent as GroupsIcon }from "../../assets/UI-icons/groups-lg.svg";
 import { ReactComponent as CalendarIcon } from "../../assets/UI-icons/calendar.svg";
 import { ReactComponent as SearchIcon } from "../../assets/UI-icons/search.svg";
+import { ReactComponent as TimesIcon } from "../../assets/UI-icons/times.svg";
 
 const Header = () => {
+    const [search, setSearch] = useState(false)
+    const [searchValue, setSearchValue] = useState("")
+    console.log(searchValue);
 
     const [selectedDay, setSelectedDay] = useState(null);
     const date = new Date()
@@ -19,16 +23,22 @@ const Header = () => {
     const renderCustomInput = ({ ref }) => (
         <input
           readOnly
-          ref={ref}
+          ref={ref} // necessary
           placeholder={`${day < 10 ? "0" + (day) : day}/${month < 10 ? "0" + (month + 1 ): month}/${year}`}
           value={selectedDay ? `${selectedDay.day < 10 ? "0" + selectedDay.day : selectedDay.day}/${ selectedDay.month < 10 ? `0${selectedDay.month}` : selectedDay.month}/${selectedDay.year}` : ''}
-          className="my-custom-input-class"
+          className="my-custom-input-class" // a styling class
         />
       )
+      
+
 
     return (
+      <>
         <div className="header_container">
-            <div className="header__categories">
+          <div className="header_wrapper" >
+            <div className="header__categories" style={
+            search ? {transitionDelay: ".4s", opacity: "0", zIndex: "-1"} : {transitionDelay: ".1s",opacity: "1", zIndex: "0"}
+          }>
             <div className="category__icon">
               <StudentIcon />
             </div>
@@ -38,7 +48,9 @@ const Header = () => {
             </div>
           </div>
           <div className="line"></div>
-          <div className="header__categories">
+          <div className="header__categories" style={
+            search ? {transitionDelay: ".3s",opacity: "0", zIndex: "-1"} : {transitionDelay: ".2s",opacity: "1", zIndex: "0"}
+          }>
             <div className="category__icon">
               <GroupsIcon />
             </div>
@@ -48,7 +60,9 @@ const Header = () => {
             </div>
           </div>
           <div className="line"></div>
-          <div className="header__categories">
+          <div className="header__categories" style={
+            search ? {transitionDelay: ".2s",opacity: "0", zIndex: "-1"} : {transitionDelay: ".3s",opacity: "1", zIndex: "0"}
+          }>
             <div className="category__icon">
             <MoneyIcon />
             </div>
@@ -57,21 +71,33 @@ const Header = () => {
               <p className="category__quantity">16ta</p>
             </div>
           </div>
-          <div className="header_calendar">
+          <div className="header_calendar" style={
+            search ? {transitionDelay: ".1s",opacity: "0", zIndex: "-1"} : {transitionDelay: ".4s",opacity: "1", zIndex: "0"}
+          }>
             <CalendarIcon />
             <DatePicker
                 calendarClassName="custom_calendar"
                 value={selectedDay}
                 onChange={setSelectedDay}
-                renderInput={renderCustomInput}
+                renderInput={renderCustomInput} // render a custom input
                 shouldHighlightWeekends
-                colorPrimary="#FF3B30"
+                colorPrimary="#FF3B30"  
             />
           </div>
-          <div className="header_search" >
-              <SearchIcon />
+          <form className="header_search" 
+            style={
+              search === true ? {position: "absolute", right: "2.5%", width: "95%", transition: ".9s"} : {position: "absolute", right: "2.5%", width: "70px", transition: ".6s", zIndex: "0"}
+            }
+          >
+              <div onClick={() => {setSearch(true)}}  style={search ? {position: "absolute", left: "1.5%"} : {position: "static", width: "60px", display: "flex", alignItems: "center", justifyContent: "center"}}><SearchIcon /></div>
+              <TimesIcon onClick={() => setSearch(false)} style={search ? {position: "absolute", right: "1.5%"} : {display: "none"}}/>
+              <input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} type="text" placeholder="O'qituvchilarni qidirish..." style={ search ? {width: "93%", height: "61px", outline: "none", border: "none", background: "#F7F7F7", fontSize: "20px", textIndent: "30px", fontFamily: "Poppins", transition: "1s"} : {width: "0",  height: "0px", outline: "none", border: "none", background: "#F7F7F7", fontSize: "20px", textIndent: "10px", position: "absolute", fontFamily: "Poppins", transition: ".1s"}}/>
+            </form>
+            <div></div>
           </div>
+          
         </div>
+      </>
     )
 }
 
