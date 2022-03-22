21594d7808a6f357112187396d83d976ae7ccb86
Umidjon Dedaxanov <macbook@MacBook-Pro-macbook.local>
2022-03-22 08:05:30 +0500

    Create_teacher

--- src/components/teacher/new-teacher/NewTeacher.js
+++ src/components/teacher/new-teacher/NewTeacher.js
@@ -1,52 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../new-teacher/Newteacher.css";
 const NewTeacher = () => {
+  const [shadow, setShadow] = useState(false)
   return (
     <div className="new__teacher__main__container">
-      <h1 className="new__teacher__title">O'qituvchi Qo'shish</h1>
-      <div className="info__wrapper">
-        <div className="teachers__information">
-          <form action="">
-            <label htmlFor="">
-              O'qituvchi Ismi<span>*</span>:
-            </label>
-            <input type="text" placeholder="O'qituvchi Ismi" required />
-            <label htmlFor="">
-              O'qituvchi Familiyasi<span>*</span>:
-            </label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-            <label htmlFor="">
-              Telefon raqam<span>*</span>:{" "}
-            </label>
-            <input type="text" placeholder="Telefon raqam" required />
-            <label htmlFor="">
-              O'qituvchi Kasbi<span>*</span>:
-            </label>
-            <input type="text" placeholder="O'qituvchi Kasbi" required />
-            <label htmlFor="">O'qituvchi Familiyasi:</label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-            <label htmlFor="">O'qituvchi Familiyasi:</label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-          </form>
-        </div>
-        <div className="teachers__information">
-          <form action="">
-            <label htmlFor="">O'qituvchi Ismi:</label>
-            <input type="text" placeholder="O'qituvchi Ismi" required />
-            <label htmlFor="">O'qituvchi Familiyasi:</label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-            <label htmlFor="">Telefon raqam: </label>
-            <input type="text" placeholder="Telefon raqam" required />
-            <label htmlFor="">O'qituvchi Kasbi:</label>
-            <input type="text" placeholder="O'qituvchi Kasbi" required />
-            <label htmlFor="">O'qituvchi Familiyasi:</label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-            <label htmlFor="">O'qituvchi Familiyasi:</label>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required />
-          </form>
-        </div>
+      <h1 style={{padding: "20px"}}>O'qituvchi qo'shish</h1>
+      <div className="registration_teacher">
+        <form>
+          
+            <p>O'qituvchi ismi <span>*</span></p>
+            <input type="text" placeholder="O'qituvchi ismi" required/>
+            <p>O'qituvchi Familiyasi <span>*</span></p>
+            <input type="text" placeholder="O'qituvchi Familiyasi" required/>
+            <p>Telefon raqam <span>*</span></p>
+            <div className="phone_number" onClick={() => {setShadow(!shadow)}} style={shadow ? {boxShadow: "#EE4236 0px 0px 10px"} : null}>
+              <input type="number" placeholder="+998 (--) --- -- --" required/>
+              <input type="number" placeholder="+998 (--) --- -- --" required/>
+            </div>
+            <p>O'qituvchi mutaxasisligi <span>*</span></p>
+            <input type="text" placeholder="O'qituvchi mutaxasisligi" required/>
+            <p>E-mail</p>
+            <input type="email" placeholder="E-mail"/>
+            <p>Telegram user name</p>
+            <input type="text" placeholder="Telegram user name"/>
+            <button>Qo'shish</button>
+          
+        </form>
       </div>
-      <button className="add__new__teacher">Yaratish</button>
     </div>
   );
 };
