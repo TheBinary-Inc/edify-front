b9017363160618bcabeba428152bc9c080f5e9a5
REACT-DEVELOPER-IBROKHIM <ibrohim123451@gmail.com>
2022-03-26 10:05:37 +0500

    Create new teacher

--- src/components/teacher/new-teacher/NewTeacher.js
+++ src/components/teacher/new-teacher/NewTeacher.js
@@ -1,31 +1,78 @@
-import React, { useState } from "react";
-import "../new-teacher/Newteacher.css";
+import React, {useState} from "react";
+import "./Newteacher.css";
+import { AiOutlineClose } from "react-icons/ai";
+
 const NewTeacher = () => {
-  const [shadow, setShadow] = useState(false)
+
+  const [tags, setTags] = useState([]);
+
+  const removeTags = (indexToRemove) => {
+    setTags([...tags.filter((_, index) => index !== indexToRemove)]);
+  };
+
+  const addTags = (e) => {
+    if (e.target.value !== "") {
+      setTags([...tags, e.target.value]);
+      e.target.value = "";
+    }
+  };
+
   return (
-    <div className="new__teacher__main__container">
-      <div className="registration_teacher">
-        <form>
-          
-            <p>O'qituvchi ismi <span>*</span></p>
-            <input type="text" placeholder="O'qituvchi ismi" required/>
-            <p>O'qituvchi Familiyasi <span>*</span></p>
-            <input type="text" placeholder="O'qituvchi Familiyasi" required/>
-            <p>Telefon raqam <span>*</span></p>
-            <div className="phone_number" onClick={() => {setShadow(!shadow)}} style={shadow ? {boxShadow: "#EE4236 0px 0px 10px"} : null}>
-              <input type="number" placeholder="+998 (--) --- -- --" required/>
-              <input type="number" placeholder="+998 (--) --- -- --" required/>
-            </div>
-            <p>O'qituvchi mutaxasisligi <span>*</span></p>
-            <input type="text" placeholder="O'qituvchi mutaxasisligi" required/>
-            <p>E-mail</p>
-            <input type="email" placeholder="E-mail"/>
-            <p>Telegram user name</p>
-            <input type="text" placeholder="Telegram user name"/>
-            <button>Qo'shish</button>
-          
-        </form>
+    <div className="newTeacher">
+     <div className="left">
+        <div className="container">
+          <p>O'qituvchi ismi va familiyasi</p>
+          <input type="text" required />
+        </div>
+        <div className="container">
+          <p>O'qituvchi telefon raqami</p>
+            <input
+              required
+              type="tel"
+              className="input_tags"
+              maxLength={13}
+              minLength={13}
+              onKeyUp={(event) =>
+                event.key === "Enter" ? addTags(event) : null } 
+              defaultValue={"+998"} />
+            <ul className="tags" >
+              {Array.isArray(tags) && tags?.map((tag, index) => {
+                return index <= 1 && tag.length === 13 ? <li key={index}>
+                  <span>{tag} </span>
+                  <AiOutlineClose onClick={() => removeTags(index)} />
+                </li> : <></>
+            })}
+            </ul>
+        </div>
+        <div className="container">
+          <p>O'qituvchi mutaxasisligi</p>
+          <input type="text"  required />
+        </div>
+        <div className="container">
+          <p>O'qituvchi manzili</p>
+          <input type="text" required />
+        </div>
+      </div> 
+      <div className="right"> 
+          <div className="container">
+            <p>O'qituvchi foydalanuchi nomi</p>
+            <input type="text" required />
+          </div>
+          <div className="container">
+            <p>O'qituvchi foydalanuchi nomi</p>
+            <input type="text" required />
+          </div>
+          <div className="container">
+            <p>O'qituvchi foydalanuchi nomi</p>
+            <input type="text" required />
+          </div>
+          <div className="container">
+            <p>O'qituvchi foydalanuchi nomi</p>
+            <input type="text" required />
+          </div>
+
       </div>
+      <button type="submit" className="add_btn">Qo'shish</button>
     </div>
   );
 };
